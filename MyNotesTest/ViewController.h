ea62f0cc4594bec4b16ffce08326cc98684a1e6d
嵇明新 <jimingxin@jimingxindemacbookair.local>
2016-04-25 11:17:22 +0800

    Initial Commit

---
+++ MyNotesTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyNotesTest
+//
+//  Created by 嵇明新 on 16/4/25.
+//  Copyright © 2016年 lanhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyNotesTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MyNotesTest
+//
+//  Created by 嵇明新 on 16/4/25.
+//  Copyright © 2016年 lanhe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
